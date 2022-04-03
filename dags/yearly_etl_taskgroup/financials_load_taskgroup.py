5956b0edb6086c9d9bc7c0c6d3ee2f4f1d4f9069
vickiyew <vickiyew@gmail.com>
2022-04-03 09:20:01 +0800

    update

--- dags/yearly_etl_taskgroup/financials_load_taskgroup.py
+++ dags/yearly_etl_taskgroup/financials_load_taskgroup.py
@@ -96,7 +96,7 @@ def build_financials_load_taskgroup(dag: DAG) -> TaskGroup:
 
     start_loading = DummyOperator(
         task_id = 'start_loading',
-        trigger_rule = 'all_success', # may need to edit this?
+        trigger_rule = 'all_success', 
         dag = dag
     )
 
