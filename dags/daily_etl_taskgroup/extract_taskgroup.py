c8675f48f3993ad9e5be323e2c6cc0701ab72bec
tohjiaxuan <61282771+tohjiaxuan@users.noreply.github.com>
2022-04-09 14:09:39 +0800

    update extract for comm

--- dags/daily_etl_taskgroup/extract_taskgroup.py
+++ dags/daily_etl_taskgroup/extract_taskgroup.py
@@ -105,6 +105,7 @@ def build_extract_taskgroup(dag: DAG) -> TaskGroup:
         df = bq_client.query(query).to_dataframe()
         recent_date = df['f0_'].values[0]
         string_date = np.datetime_as_string(recent_date, unit='D')
+        print('Latest date:',string_date)
         return string_date
 
     ###########################
