f88da8d10abcd35543e58e278f5c74ef4f4e0c31
tohjiaxuan <61282771+tohjiaxuan@users.noreply.github.com>
2022-04-12 14:12:20 +0800

    archive latest update before 12apr

---
+++ dags/daily_etl_taskgroup/12 Apr/daily_dag.py
@@ -0,0 +1,79 @@
+from airflow.contrib.operators.gcs_to_bq import GoogleCloudStorageToBigQueryOperator
+from airflow.contrib.operators.bigquery_operator import BigQueryOperator
+from airflow.models import DAG
+from airflow.models import TaskInstance
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.python import BranchPythonOperator
+from airflow.operators.subdag import SubDagOperator
+from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
+from google.cloud import bigquery
+from google.cloud import storage
+from pandas_datareader import data as pdr
+
+from airflow.utils.task_group import TaskGroup
+from financials_schema_taskgroup import build_financials_schema_taskgroup
+from extract_taskgroup import build_extract_taskgroup
+from gcs_taskgroup import build_gcs_taskgroup
+from stage_taskgroup import build_stage_taskgroup
+from transform_taskgroup import build_transform_taskgroup
+from load_taskgroup import build_load_taskgroup
+
+import json
+import os
+import numpy as np
+import pandas as pd
+import pandas_ta as ta
+import requests
+import urllib.request
+import yfinance as yf 
+
+yf.pdr_override()
+
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.109 Safari/537.36',
+      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'}
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/home/airflow/airflow/dags/stockprediction_servicekey.json'
+storage_client = storage.Client()
+bucket = storage_client.get_bucket('stock_prediction_is3107')
+STAGING_DATASET = 'stock_prediction_staging_dataset'
+PROJECT_ID = 'stockprediction-344203'
+DWH_DATASET = 'stock_prediction_datawarehouse'
+
+curr_date = datetime.today().strftime('%Y-%m-%d')
+
+# Add retry_delay later on when pipeline is up
+default_args = {
+    'owner': 'Nicole',
+    'depends_on_past': False,
+    'email': ['nicole@png.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1, # Remember to change this when the actual project kickstarts
+    'retry_delay': timedelta(minutes=2),
+    'start_date': datetime(2022, 3, 15)
+}
+
+with DAG(
+    dag_id="daily_dag",
+    schedule_interval="@daily",
+    description = 'DAG for creation of data warehouse (Daily)',
+    default_args=default_args,
+    catchup = False
+) as dag:
+    with TaskGroup("tg0", prefix_group_id = False) as section_0:
+        financials_schema_taskgroup = build_financials_schema_taskgroup(dag=dag)
+    with TaskGroup("extract", prefix_group_id = False) as section_1:
+        extract_taskgroup = build_extract_taskgroup(dag=dag)
+    with TaskGroup("gcs", prefix_group_id = False) as section_2:
+        gcs_taskgroup = build_gcs_taskgroup(dag=dag)
+    with TaskGroup("stage", prefix_group_id = False) as section_3:
+        stage_taskgroup = build_stage_taskgroup(dag=dag)
+    with TaskGroup("transform", prefix_group_id = False) as section_4:
+        transform_taskgroup = build_transform_taskgroup(dag=dag)
+    with TaskGroup("load", prefix_group_id = False) as section_5:
+        load_taskgroup = build_load_taskgroup(dag=dag)
+
+    section_0 >> section_1 >> section_2 >> section_3 >> section_4 >> section_5
---
+++ dags/daily_etl_taskgroup/12 Apr/transform_taskgroup.py
@@ -0,0 +1,434 @@
+from airflow.contrib.operators.gcs_to_bq import GoogleCloudStorageToBigQueryOperator
+from airflow.contrib.operators.bigquery_operator import BigQueryOperator
+from airflow.models import DAG
+from airflow.models import TaskInstance
+from airflow.operators.python_operator import PythonOperator
+from google.cloud import bigquery
+from google.cloud import storage
+from google.cloud.exceptions import NotFound
+from airflow.utils.task_group import TaskGroup
+
+import json
+import os
+import pandas as pd
+import pandas_ta as ta
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/home/airflow/airflow/dags/stockprediction_servicekey.json'
+storage_client = storage.Client()
+bq_client = bigquery.Client()
+bucket = storage_client.get_bucket('stock_prediction_is3107')
+STAGING_DATASET = 'stock_prediction_staging_dataset'
+PROJECT_ID = 'stockprediction-344203'
+DWH_DATASET = 'stock_prediction_datawarehouse'
+
+
+def build_transform_taskgroup(dag: DAG) -> TaskGroup:
+    transform_taskgroup = TaskGroup(group_id = 'transform_taskgroup')
+
+    ############################
+    # Define Python Functions  #
+    ############################
+
+    # Define python functions for stock prices related items (stock prices, exchange rate, interest rate)
+    def if_f_stock_exists():
+        try:
+            bq_client = bigquery.Client()
+            query = 'select COUNT(`Date`) from `stockprediction-344203.stock_prediction_datawarehouse.F_STOCKS`'
+            df = bq_client.query(query).to_dataframe()
+            df_length = df['f0_'].values[0]
+            if (df_length != 0):
+                return True
+            else:
+                return False
+        except:
+            return False
+    
+    def query_stock_dwh():
+        bq_client = bigquery.Client()
+        query = """SELECT Date, Open, High, Low, Close, Volume, Dividends, Stock_Splits, Stock
+        FROM `stockprediction-344203.stock_prediction_datawarehouse.F_STOCKS` ORDER BY `Date` DESC LIMIT 6000"""
+        df = bq_client.query(query).to_dataframe()
+
+        return df
+
+    def helper_sma_prices(input_df):
+        # Obtain unique values
+        uniq_stocks = input_df['Stock'].unique()
+        
+        sma_stocks = []
+        # Check for null values, if present, do ffill
+        for ticker in uniq_stocks:
+            print("Currently handling", ticker)
+            curr_df = input_df.loc[input_df['Stock'] == ticker]
+
+            # Sort by date
+            curr_df = curr_df.sort_values(by = ['Date'])
+            curr_df = curr_df.reset_index(drop = True)
+
+            # Forward fill values
+            if (curr_df.isnull().values.any() == True):
+                curr_df = curr_df.fillna(method='ffill')
+                    
+            # Conduct TA analysis
+            curr_df['SMA_50'] = ta.sma(curr_df['Close'], length = 50, append=True)
+            curr_df['SMA_200'] = ta.sma(curr_df['Close'], length=200, append=True)
+            curr_df["GC"] = ta.sma(curr_df['Close'], length = 50, append=True) > ta.sma(curr_df['Close'], length=200, append=True)
+            curr_df["DC"] = ta.sma(curr_df['Close'], length = 50, append=True) > ta.sma(curr_df['Close'], length=200, append=True)
+            sma_stocks.append(curr_df)
+        
+        # Transform to df
+        final_df = pd.concat(sma_stocks, ignore_index = True)
+
+        # Add Constant column for fact table later on
+        final_df['Price Category'] = "Stock"
+            
+        print("Transforming Stocks Data Complete")
+        return final_df
+
+    def query_stage_table():
+        query = "select * from `stockprediction-344203.stock_prediction_staging_dataset.distinct_hist_stock_prices`"
+        stage_df = bq_client.query(query).to_dataframe()
+
+        return stage_df
+    
+    def query_int_stage():
+        bq_client = bigquery.Client()
+        query = """SELECT * FROM `stockprediction-344203.stock_prediction_staging_dataset.distinct_interest_rate`
+        ORDER BY `Date`"""
+        df = bq_client.query(query).to_dataframe()
+        df.rename(columns={'Date':'Actual Date'}, inplace=True)
+
+        return df
+    
+    def query_ex_stage():
+        bq_client = bigquery.Client()
+        query = """SELECT `Date` FROM `stockprediction-344203.stock_prediction_staging_dataset.distinct_exchange_rate`
+        ORDER BY `Date`"""
+        df = bq_client.query(query).to_dataframe()
+
+        return df
+
+    def update_sma():
+        new_df = query_stage_table()
+        old_df = query_stock_dwh()
+
+        df = old_df.append(new_df, ignore_index = True)
+
+        uniq_dates = new_df['Date'].unique()
+        output = helper_sma_prices(df)
+        output = output[output['Date'].isin(uniq_dates)]
+        return output
+
+    def sma_prices():
+        check_dwh = if_f_stock_exists()
+        if check_dwh:
+            result = update_sma()
+        else:
+            temp = query_stage_table()
+            result = helper_sma_prices(temp)
+        print(result)
+        result.to_parquet('gs://stock_prediction_is3107/final_stock.parquet', engine='pyarrow', index=False)
+    
+    def lag_int():
+        int_df = query_int_stage()
+        ex_df = query_ex_stage()
+
+        # To ensure index is the same
+        int_df = int_df.reset_index(drop=True)
+        ex_df = ex_df.reset_index(drop=True)
+        
+        if (len(ex_df) < len(int_df)):
+            int_df = int_df.iloc[1:]
+        
+        lag_int = int_df.join(ex_df)         
+        lag_int.to_parquet('gs://stock_prediction_is3107/lag_interest.parquet', engine='pyarrow', index=False)
+
+    # Define python functions for commodities related items (gold, silver, crude oil)
+    def if_d_commodities_exists():
+        try:
+            bq_client = bigquery.Client()
+            query = 'select COUNT(`Date`) from `stockprediction-344203.stock_prediction_datawarehouse.D_COMMODITIES`'
+            df = bq_client.query(query).to_dataframe()
+            df_length = df['f0_'].values[0]
+            if (df_length != 0):
+                return True
+            else:
+                return False
+        except:
+            return False
+
+    def query_commodities_dwh():
+        bq_client = bigquery.Client()
+        query = """SELECT *
+        FROM `stockprediction-344203.stock_prediction_datawarehouse.D_COMMODITIES` ORDER BY `Date` DESC"""
+        df = bq_client.query(query).to_dataframe()
+
+        return df
+
+    def query_commodities_table(): # - ADDED
+        gold_query = "select distinct * from `stockprediction-344203.stock_prediction_staging_dataset.distinct_gold`"
+        gold_stage_df = bq_client.query(gold_query).to_dataframe()
+
+        silver_query = "select distinct * from `stockprediction-344203.stock_prediction_staging_dataset.distinct_silver`"
+        silver_stage_df = bq_client.query(silver_query).to_dataframe()
+
+        crude_oil_query = "select distinct * from `stockprediction-344203.stock_prediction_staging_dataset.distinct_crude_oil`"
+        crude_oil_stage_df = bq_client.query(crude_oil_query).to_dataframe()
+
+        return gold_stage_df, silver_stage_df, crude_oil_stage_df
+
+    """def query_commodities_table():
+        gold_query = "select * from `stockprediction-344203.stock_prediction_staging_dataset.init_gold`"
+        gold_stage_df = bq_client.query(gold_query).to_dataframe()
+
+        silver_query = "select * from `stockprediction-344203.stock_prediction_staging_dataset.init_silver`"
+        silver_stage_df = bq_client.query(silver_query).to_dataframe()
+
+        crude_oil_query = "select * from `stockprediction-344203.stock_prediction_staging_dataset.init_crude_oil`"
+        crude_oil_stage_df = bq_client.query(crude_oil_query).to_dataframe()
+
+        return gold_stage_df, silver_stage_df, crude_oil_stage_df"""
+
+    def consolidate_commodities(gold_df, silver_df, crude_oil_df): # - ADDED
+        # Remove duplicates
+        gold_df.drop_duplicates(inplace=True)
+        silver_df.drop_duplicates(inplace=True)
+        crude_oil_df.drop_duplicates(inplace=True)
+
+        # Add Constant column for fact table later on
+        gold_df['Price Category'] = "Gold"
+        silver_df['Price Category'] = "Silver"
+        crude_oil_df['Price Category'] = "Crude Oil"
+
+        # Concatenate all rows
+        commodities = []
+        commodities.append(gold_df)
+        commodities.append(silver_df)
+        commodities.append(crude_oil_df)
+        final_df = pd.concat(commodities, ignore_index = True)
+
+        # Sort by date and price category
+        final_df = final_df.sort_values(by = ['Date', 'Price Category'])
+        final_df = final_df.reset_index(drop = True)
+        return final_df
+
+    """def consolidate_commodities(gold_df, silver_df, crude_oil_df):
+        # Remove duplicates
+        gold_df.drop_duplicates(inplace=True)
+        silver_df.drop_duplicates(inplace=True)
+        crude_oil_df.drop_duplicates(inplace=True)
+
+        # Add Constant column for fact table later on
+        gold_df['Price Category'] = "Gold"
+        silver_df['Price Category'] = "Silver"
+        crude_oil_df['Price Category'] = "Crude Oil"
+
+        # Concatenate all rows
+        commodities = []
+        commodities.append(gold_df)
+        commodities.append(silver_df)
+        commodities.append(crude_oil_df)
+        final_df = pd.concat(commodities, ignore_index = True)
+
+        # Sort by date and price category
+        final_df = final_df.sort_values(by = ['Date', 'Price Category'])
+        final_df = final_df.reset_index(drop = True)
+        return final_df"""
+
+    def transform_commodities():
+        gold_df, silver_df, crude_oil_df = query_commodities_table()
+        final_df = consolidate_commodities(gold_df, silver_df, crude_oil_df)
+        return final_df
+
+    def update_commodities(new_df):
+        commodities_temp = []
+        old_df = query_commodities_dwh()
+        old_df.rename({'Price_Category': 'Price Category'}, axis=1, inplace=True)
+        commodities_temp.append(old_df)
+        commodities_temp.append(new_df)
+        updated_df = pd.concat(commodities_temp, ignore_index = True)
+        updated_df.drop_duplicates(inplace=True)
+        return updated_df
+
+    def commodities():
+        df = transform_commodities()
+        check_dwh = if_d_commodities_exists() 
+        if check_dwh:
+            result = update_commodities(df)
+        else:
+            result = df
+        print(result.head())
+        result.to_parquet('gs://stock_prediction_is3107/final_commodities.parquet', engine='pyarrow', index=False)
+  
+    ############################
+    # Define Airflow Operators #
+    ############################
+
+    #############################
+    # Transformation in Staging #
+    #############################
+    # Remove Duplicates exchange rate
+    distinct_exchange = BigQueryOperator(
+        task_id = 'distinct_exchange_task',
+        use_legacy_sql = False,
+        sql = f'''
+                create or replace table `{PROJECT_ID}.{STAGING_DATASET}.distinct_exchange_rate`
+                as select distinct parse_timestamp('%Y-%m-%d', end_of_day) as Date,
+                concat(temp.end_of_day, '-EXR') as EXR_ID, *
+                except (
+                    end_of_day, preliminary, timestamp
+                ),
+                preliminary as ex_rate_preliminary, timestamp as ex_rate_timestamp
+                from `{PROJECT_ID}.{STAGING_DATASET}.init_exchange_rates` as temp
+        ''',
+        dag = dag,
+    )
+
+    # Reformat and remove duplicates interest rate
+    distinct_interest = BigQueryOperator(
+        task_id = 'distinct_interest_task',
+        use_legacy_sql = False,
+        sql = f'''
+        create or replace table `{PROJECT_ID}.{STAGING_DATASET}.distinct_interest_rate` 
+        as select distinct parse_timestamp('%Y-%m-%d', end_of_day) as Date, 
+        concat(temp.end_of_day, '-INR') as INR_ID, *
+        except(
+            end_of_day, preliminary, timestamp,
+            interbank_overnight, interbank_1w, interbank_1m, interbank_2m, interbank_3m,
+            interbank_6m, interbank_12m, commercial_bills_3m, usd_sibor_3m, sgs_repo_overnight_rate
+        ),
+        preliminary as int_rate_preliminary, timestamp as int_rate_timestamp
+        from `{PROJECT_ID}.{STAGING_DATASET}.init_interest_rates` as temp
+        ''',
+        dag = dag
+    )
+
+    # Remove Duplicates
+    distinct_stock_prices = BigQueryOperator(
+        task_id = 'distinct_stock_prices_task',
+        use_legacy_sql = False,
+        sql = f'''
+        create or replace table `{PROJECT_ID}.{STAGING_DATASET}.distinct_hist_stock_prices` 
+        as select distinct *
+        from `{PROJECT_ID}.{STAGING_DATASET}.init_hist_stock_prices`
+        ''',
+        dag = dag
+    )
+
+    # Remove Duplicates gold - ADDED
+    distinct_gold = BigQueryOperator(
+        task_id = 'distinct_gold_task',
+        use_legacy_sql = False,
+        sql = f'''
+                create or replace table `{PROJECT_ID}.{STAGING_DATASET}.distinct_gold`
+                as select distinct concat( format_timestamp('%Y-%m-%d', temp.Date) , '-GOLD-COMM') as COMM_ID, *
+                from `{PROJECT_ID}.{STAGING_DATASET}.init_gold` as temp
+        ''',
+        dag = dag
+    )
+
+    # Remove Duplicates silver - ADDED
+    distinct_silver = BigQueryOperator(
+        task_id = 'distinct_silver_task',
+        use_legacy_sql = False,
+        sql = f'''
+                create or replace table `{PROJECT_ID}.{STAGING_DATASET}.distinct_silver`
+                as select distinct concat( format_timestamp('%Y-%m-%d', temp.Date) , '-SILVER-COMM') as COMM_ID, *
+                from `{PROJECT_ID}.{STAGING_DATASET}.init_silver` as temp
+        ''',
+        dag = dag
+    )
+
+    # Remove Duplicates crude oil - ADDED
+    distinct_crude_oil = BigQueryOperator(
+        task_id = 'distinct_crude_oil_task',
+        use_legacy_sql = False,
+        sql = f'''
+                create or replace table `{PROJECT_ID}.{STAGING_DATASET}.distinct_crude_oil`
+                as select distinct concat( format_timestamp('%Y-%m-%d', temp.Date) , '-CRUDE OIL-COMM') as COMM_ID, *
+                from `{PROJECT_ID}.{STAGING_DATASET}.init_crude_oil` as temp
+        ''',
+        dag = dag
+    )
+
+    """# Create id for commodities
+    distinct_commodities = BigQueryOperator(
+        task_id = 'distinct_commodities_task',
+        use_legacy_sql = False,
+        sql = f'''
+        create or replace table `{PROJECT_ID}.{STAGING_DATASET}.final_commodity_prices` 
+        as select distinct
+        concat( FORMAT_TIMESTAMP('%Y-%m-%d', temp.Date) , '-', temp.Price_Category, '-COMM') as COMM_ID, *
+        from `{PROJECT_ID}.{STAGING_DATASET}.combined_commodity_prices` as temp
+        ''',
+        dag = dag
+    )"""
+
+    # Add SMA to df
+    sma_stock = PythonOperator(
+        task_id = 'sma_stock_task',
+        python_callable = sma_prices,
+        dag = dag
+    )
+
+    # Add lag dates to df
+    lag_int = PythonOperator(
+        task_id = 'lag_int_task',
+        python_callable = lag_int,
+        dag = dag
+    )   
+
+    # Load sma data from GCS to BQ
+    load_sma = GoogleCloudStorageToBigQueryOperator(
+        task_id = 'stage_sma_task',
+        bucket = 'stock_prediction_is3107',
+        source_objects = ['final_stock.parquet'],
+        destination_project_dataset_table = f'{PROJECT_ID}:{STAGING_DATASET}.final_hist_prices',
+        write_disposition='WRITE_TRUNCATE',
+        autodetect = True,
+        source_format = 'PARQUET',
+        dag = dag
+    )
+
+    # Load lag interest date data from GCS to BQ
+    load_lag_interest = GoogleCloudStorageToBigQueryOperator(
+        task_id = 'stage_lag_int_task',
+        bucket = 'stock_prediction_is3107',
+        source_objects = ['lag_interest.parquet'],
+        destination_project_dataset_table = f'{PROJECT_ID}:{STAGING_DATASET}.final_interest_rate',
+        write_disposition='WRITE_TRUNCATE',
+        autodetect = True,
+        source_format = 'PARQUET',
+        dag = dag
+    )
+
+    # Combine gold, silver and crude oil dataframes into a commodities dataframe
+    combine_commodities = PythonOperator(
+        task_id = 'commodities_task',
+        python_callable = commodities,
+        dag = dag
+    )
+
+    # Load commodities data from GCS to BQ
+    load_commodities = GoogleCloudStorageToBigQueryOperator(
+        task_id = 'stage_commodities_task',
+        bucket = 'stock_prediction_is3107',
+        source_objects = ['final_commodities.parquet'],
+        destination_project_dataset_table = f'{PROJECT_ID}:{STAGING_DATASET}.final_commodity_prices',
+        write_disposition='WRITE_TRUNCATE',
+        autodetect = True,
+        source_format = 'PARQUET',
+        dag = dag
+    )    
+
+    ############################
+    # Define Tasks Hierarchy   #
+    ############################
+    [distinct_exchange, distinct_interest, distinct_stock_prices, distinct_gold, distinct_silver, distinct_crude_oil] 
+    distinct_stock_prices >> sma_stock >> load_sma
+    distinct_interest >> lag_int >> load_lag_interest
+    [distinct_exchange, load_lag_interest, load_sma] 
+    [distinct_gold, distinct_silver, distinct_crude_oil] >> combine_commodities >> load_commodities # - ADDED
+    #combine_commodities >> load_commodities >> distinct_commodities
+
+    return transform_taskgroup
